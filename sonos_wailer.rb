f97c1d656da7c083a0a52f796a1942c27a9c748b
Chris Hollindale <chris.hollindale@gmail.com>
2014-08-30 20:24:43 -0700

    Write Sonos wailer script

---
+++ sonos_wailer.rb
@@ -0,0 +1,21 @@
+require 'rubygems'
+require 'sonos'
+
+def queue_bob(speaker)
+  speaker.add_to_queue "x-sonos-spotify:spotify%3atrack%3a1Bh3GMDtYN2FB4jWp7VlrZ?sid=12&amp;flags=32"
+end
+
+system = Sonos::System.new
+upstairs_speaker = system.speakers.select{ |speaker| speaker.name == "Upstairs" }.first
+
+# Bob Marley and the Wailers - Get Up, Stand Up - Spotify
+if upstairs_speaker.is_playing?
+  queue_bob(upstairs_speaker)
+  upstairs_speaker.next
+else
+  upstairs_speaker.clear_queue
+  queue_bob(upstairs_speaker)
+  upstairs_speaker.play
+end
+
+
