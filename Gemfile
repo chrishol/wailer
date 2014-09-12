3b327492c6a8a1788d0cbd7c0a6366a681c6c3b7
Chris Hollindale <chris.hollindale@gmail.com>
2014-09-11 19:00:44 -0700

    Add really stupid engineering standup triggerer

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 gem 'sonos',          '~> 0.3.6'
 gem 'slack-notifier', '~> 0.5.0'
+gem 'faker'
---
+++ slack_engineering_wailer.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'slack-notifier'
+require 'yaml'
+require 'net/http'
+require 'json'
+require 'faker'
+
+def random_name
+  [Faker::Name.prefix,Faker::Name.first_name,Faker::Name.last_name,Faker::Name.suffix].join(" ")
+end
+
+def random_gif
+  url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cinemagraphs"
+  resp = Net::HTTP.get_response(URI.parse(url))
+  buffer = resp.body
+  result = JSON.parse(buffer) 
+  puts result
+  return result["data"]["image_url"]
+end
+
+def random_emoji
+  File.open('emoji.txt') do |f|
+    return f.each_line.to_a.sample.strip
+  end
+end
+
+config = YAML.load_file("slack.yml")
+
+notifier = Slack::Notifier.new config["account_name"], config["webhook_token"], channel: "#engineering", username: random_name
+notifier.ping "@chris Time for the engineering standup\r\n#{random_gif}", icon_emoji: ":#{random_emoji}:", link_names: 1
