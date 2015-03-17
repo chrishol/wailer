4f90bb5de6a463dd16dc845d0d6bf622738d248c
Chris Hollindale <chris.hollindale@gmail.com>
2015-03-16 17:04:31 -0700

    Change scheduling

--- slack_wailer.rb
+++ slack_wailer.rb
@@ -4,5 +4,5 @@ require 'yaml'
 
 config = YAML.load_file("slack.yml")
 
-notifier = Slack::Notifier.new config["account_name"], config["webhook_token"], channel: "#office", username: "Standup Panda"
-notifier.ping "@channel IT'S STANDUP TIME!", icon_emoji: ":panda_face:", link_names: 1
+notifier = Slack::Notifier.new config["account_name"], config["webhook_token"], channel: "#office", username: "Panda"
+notifier.ping "@channel It's time for All Minds!", icon_emoji: ":panda_face:", link_names: 1
