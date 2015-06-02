cf257081a73e2aded3da60b7b9e6cb1f93fbc2c9
Chris Hollindale <chris.hollindale@gmail.com>
2015-06-02 11:24:26 -0700

    Update Engineering Wailer text and channel

--- slack_engineering_wailer.rb
+++ slack_engineering_wailer.rb
@@ -13,7 +13,7 @@ def random_gif
   url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cinemagraphs"
   resp = Net::HTTP.get_response(URI.parse(url))
   buffer = resp.body
-  result = JSON.parse(buffer) 
+  result = JSON.parse(buffer)
   puts result
   return result["data"]["image_url"]
 end
@@ -26,5 +26,12 @@ end
 
 config = YAML.load_file("slack.yml")
 
-notifier = Slack::Notifier.new config["account_name"], config["webhook_token"], channel: "#engineering", username: random_name
-notifier.ping "@channel Time for the engineering standup\r\n#{random_gif}", icon_emoji: ":#{random_emoji}:", link_names: 1
+notifier = Slack::Notifier.new(
+  config["account_name"],
+  config["webhook_token"],
+  channel: "#tech_team",
+  username: random_name
+)
+notifier.ping "@channel Reminder: Please write your standup updates by 10:30am!\r\n#{random_gif}",
+              icon_emoji: ":#{random_emoji}:",
+              link_names: 1
